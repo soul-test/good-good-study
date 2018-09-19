84f9430702e3596a240887ac26c518dfe7e465b8
littlelu0 <992021095@qq.com>
2018-09-19 15:16:09 +0800

    99cfb
    Signed-off-by: littlelu0 <992021095@qq.com>

---
+++ xiaojiejie/cf.py
@@ -0,0 +1,13 @@
+
+i = 1
+
+while i <= 9 :
+   j = 1
+   while j <= i :
+        sum = 1
+        sum = i * j
+        print(str(i), "*", str(j), "= ", str(sum),end="  ")
+        j += 1
+   print()
+   i += 1  
+    
