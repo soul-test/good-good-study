6703876460cdff9b83eb1777b74033c290655f5a
Linna520 <2472471999@qq.com>
2018-09-19 15:21:52 +0800

    This is my first python
    Signed-off-by: Linna520 <2472471999@qq.com>

--- yangyakun/猜数字游戏.py
+++ yangyakun/猜数字游戏.py
@@ -6,12 +6,12 @@ count = 1
 while count <= 5:
 	guess = int(input('请您先输入一个数字: '))
 	if guess == sj:
-		print('你真棒！')
+		print('你真棒!')
 		break
 	elif guess < sj:
-		print('数字有点小！')
+		print('数字有点小!')
 	else:
-		print('数字有点大！')
+		print('数字有点大!')
 	count = count + 1
 	if count == 6:
-		print('对不起，您的智商有限，不适合此类游戏')
+		print('对不起，您的智商有限，不适合此类游戏，谢谢参与!')
