8be2a740425719bba183642ffc18abe145ecf83c
root <root@www.test.com>
2018-09-19 23:45:58 +0800

    猜大小
    Signed-off-by: root <root@www.test.com>

--- luowei/01.py
+++ luowei/01.py
@@ -1,9 +1,16 @@
-m = 1
-while m <= 9:
-    i = 1
-    while i <= m:
-        print(str(i) + '*' + str(m) + '=' + str(i*m),end="\t")
-        i += 1
-    print()
-    m +=1
+import random
+sj = random.randint(0,100)
+count = 1
+while   count <= 5:
+    guess = int(input('请输入一个数字:'))
+    if guess == sj:
+        print('你真棒:')
+        break
+    elif guess < sj:
+        print('数字猜小了:')
+    else:
+        print('s数字猜大了:')
+    count += 1
+    if count == 6:
+        print('太笨了,请重新投币!')
 
