c90d48d9b18378ea64293dc18c82d7d389aae79f
root <root@localhost.localdomain>
2018-09-20 20:04:41 +0800

    小能猫
    Signed-off-by: root <root@localhost.localdomain>

--- CIA-pt/4.py
+++ CIA-pt/4.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF-8 -*-
  
-# Filename : test.py
-# author by : www.runoob.com
+# Filename : 4.py
+
  
-# 九九乘法表
+#This is my 九九乘法表
 for i in range(1, 10):
     for j in range(1, i+1):
         print('{}x{}={}\t'.format(i, j, i*j), end='')
