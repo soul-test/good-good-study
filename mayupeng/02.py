0a6a7ba9c3b7c395cf0a56e0de0c85ad3b6c6407
root <root@localhost.localdomain>
2018-09-20 19:36:41 +0800

    显示你输入的所有东西
    Signed-off-by: root <root@localhost.localdomain>

---
+++ mayupeng/02.py
@@ -0,0 +1,7 @@
+name = input('请输入name:')
+age = input('请输入age:')
+height = input('请输入height:')
+weight = input('请输入weight:')
+gender = input('请输入gender:')
+print("你输入的名字是:", name, "你输入的年龄是:", age, "你输入的身高是:" ,height, "输入的体重是:" ,weight, "你输入的性别是:", gender)
+
